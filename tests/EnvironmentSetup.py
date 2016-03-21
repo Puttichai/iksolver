cfbf509a19c6e52c8ad3a101947a08adc83a08eb
Puttichai <L.Puttichai@gmail.com>
2016-03-21 14:19:04 +0800

    added a collision checker

--- tests/EnvironmentSetup.py
+++ tests/EnvironmentSetup.py
@@ -1,5 +1,5 @@
 import unittest
-from openravepy import Environment
+import openravepy as orpy
 
 import rospkg
 rospack = rospkg.RosPack()
@@ -8,12 +8,14 @@ rospack = rospkg.RosPack()
 class OpenRAVEsetup(unittest.TestCase):
 
     def setUp(self):
-        self.env = Environment()
+        self.env = orpy.Environment()
 
         # Get the DensoVS60 model - fast loading time
         robot_path = rospack.get_path('cribus_openrave') + '/robots/denso-vs060.zae'
         self.robot = self.env.ReadRobotURI(robot_path)
         self.env.Add(self.robot, True)
+        collisionchecker = orpy.RaveCreateCollisionChecker(self.env, 'ode')
+        self.env.SetCollisionChecker(collisionchecker)
         self.robot.SetName('Denso')
         self.manip = self.robot.GetManipulator('Flange')
 
