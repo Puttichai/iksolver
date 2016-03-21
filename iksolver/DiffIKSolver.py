346ce58c3bc5f18ecb48febf17992047d4a9e40d
Puttichai <L.Puttichai@gmail.com>
2016-03-21 14:18:44 +0800

    separated iksolver5d

--- iksolver/DiffIKSolver.py
+++ iksolver/DiffIKSolver.py
@@ -14,21 +14,17 @@ class DiffIKSolver(object):
         self.qd_max = +qd_lim*np.ones(n)
 
 
-    def solve(self, targetpose, q, dt, max_it=1000, conv_tol=1e-5):
+    def solve(self, targetpose, q, dt, max_it=1000, conv_tol=1e-6):
         """
         Compute joint values which bring the manipulator to T.
 
         T -- desired manipulator transformation
         q0 -- initial guess
         max_it -- maximum number of differential IK iterations
-        conv_tol -- if the objective dwindles by less than this
-                    threshold after a differential IK step, we assume
-                    the solver has converged to the best solution it
-                    can find
+        conv_tol -- if all elements of the actual pose is not farther 
+                    from their corresponding elements in the target pose
+                    than conv_tol, the solver has already converged
         """
-        # R = T[0:3, 0:3]
-        # p = T[0:3, 3]
-        # targetpose = np.hstack([orpy.quatFromRotationMatrix(R), p])
         if targetpose[0] < 0:
             targetpose[0:4] *= -1.
 
@@ -38,9 +34,15 @@ class DiffIKSolver(object):
 
         while it < max_it:
             it += 1
-            prev_obj = cur_obj
-            cur_obj = self._eval_objective(targetpose, q)
-            if abs(cur_obj - prev_obj) < conv_tol and cur_obj < conv_tol:
+            # prev_obj = cur_obj
+            # cur_obj = self._eval_objective(targetpose, q)
+            # if abs(cur_obj - prev_obj) < conv_tol and cur_obj < conv_tol:
+            #     # local minimum reached
+            #     reached = True
+            #     break
+            
+            pose_actual = self._get_pose(q)
+            if np.allclose(targetpose, pose_actual, atol=conv_tol, rtol=0):
                 # local minimum reached
                 reached = True
                 break
@@ -56,9 +58,6 @@ class DiffIKSolver(object):
 
 
     def _eval_error(self, x0, x1):
-        # err = np.zeros(7)
-        # err[:4] = orpy.quatMultiply(x0[:4], orpy.quatInverse(x1[:4]))
-        # err[4:] = x0[4:] - x1[4:]
         return x0 - x1
 
 
@@ -100,144 +99,4 @@ class DiffIKSolver(object):
         except:
             qd = np.dot(np.linalg.pinv(J), pose_error)
         return qd
-
             
-class DiffIKSolver5D(object):
-    def __init__(self, robot, manipulatorname, qd_lim=1):
-        self.robot = robot
-        self.manip = robot.SetActiveManipulator(manipulatorname)
-        n = self.robot.GetActiveDOF()
-        self.q_min = self.robot.GetActiveDOFLimits()[0]
-        self.q_max = self.robot.GetActiveDOFLimits()[1]
-        self.qd_min = -qd_lim*np.ones(n)
-        self.qd_max = +qd_lim*np.ones(n)
-
-
-    def solve(self, targetpose, q, dt, max_it=1000, conv_tol=1e-5):
-        """
-        Compute joint values which bring the manipulator to T.
-
-        T -- desired manipulator transformation
-        q0 -- initial guess
-        max_it -- maximum number of differential IK iterations
-        conv_tol -- if the objective dwindles by less than this
-                    threshold after a differential IK step, we assume
-                    the solver has converged to the best solution it
-                    can find
-        """
-        if targetpose[0] < 0:
-            targetpose[0:4] *= -1.
-
-        cur_obj = 1000. # some arbitrary number
-        it = 0 # iteration counter
-        reached = False
-
-        while it < max_it:
-            it += 1
-            prev_obj = cur_obj
-            cur_obj = self._eval_objective(targetpose, q)
-
-            cos_alpha_desired = targetpose[0]
-            sin_alpha_desired = np.sqrt(1 - cos_alpha_desired**2)
-            if abs(sin_alpha_desired) > 1e-8:
-                direction_desired = targetpose[1:4] / sin_alpha_desired
-            else:
-                direction_desired = np.zeros(3)
-
-            pose_actual = self._get_pose(q)
-            cos_alpha_actual = pose_actual[0]
-            sin_alpha_actual = np.sqrt(1 - cos_alpha_actual**2)
-            if abs(sin_alpha_actual) > 1e-8:
-                direction_actual = pose_actual[1:4] / sin_alpha_actual
-            else:
-                direction_actual = np.zeros(3)
-                
-            if (np.allclose(direction_desired, direction_actual,
-                            rtol=0*1e-8, atol=1e-6) and
-                np.allclose(targetpose[4:], pose_actual[4:],
-                            rtol=0*1e-8, atol=1e-6)):
-                # local minimum reached
-                reached = True
-                break
-
-            qd = self._compute_velocity(targetpose, q)
-            qd = np.maximum(np.minimum(qd, self.qd_max), self.qd_min)
-            q = q + (dt * qd)
-            q = np.maximum(np.minimum(q, self.q_max), self.q_min)
-
-        if not reached:
-            print '[solve] max iteration ({0}) exceeded'.format(it)
-        return [reached, it, q]
-
-
-    def _eval_objective(self, x, q):
-        currentpose = self._get_pose(q)
-        error_rot = self._eval_error_rotation(x, currentpose)
-        error_pos = self._eval_error_position(x, currentpose)
-        
-        return 0.5*(np.dot(error_rot, error_rot) + np.dot(error_pos, error_pos))
-
-
-    def _eval_error_rotation(self, x, currentpose):
-        cos_alpha1 = currentpose[0]
-        sin_alpha1 = np.sqrt(1 - cos_alpha1**2)
-        if abs(sin_alpha1) > 1e-8:
-            w_current = currentpose[1:4]/sin_alpha1
-        else:
-            w_current = np.zeros(3)
-
-        cos_alpha2 = x[0]
-        sin_alpha2 = np.sqrt(1 - cos_alpha2**2)
-        if abs(sin_alpha2) > 1e-8:
-            w_desired = x[1:4]/sin_alpha2
-        else:
-            w_desired = np.zeros(3)
-        w_desired = x[1:4]/sin_alpha2
-
-        error_rot = w_desired - w_current
-        return error_rot
-
-    
-    def _eval_error_position(self, x, currentpose):
-        error_pos = x[4:] - currentpose[4:]
-        return error_pos
-
-
-    def _get_pose(self, q):
-        with self.robot:
-            try:
-                self.robot.SetActiveDOFValues(q)
-            except:
-                print q
-                self.robot.SetActiveDOFValues(q)
-            pose = self.manip.GetTransformPose()
-        # Convention: cos(alpha) > 0
-        # (from Stephane) this convention enforces Slerp shortest path
-        if pose[0] < 0:
-            pose[:4] *= -1
-        return pose
-
-
-    def _compute_velocity(self, targetpose, q):
-        with self.robot:
-            self.robot.SetActiveDOFValues(q)
-            currentpose = self.manip.GetTransformPose()
-            # Jacobian
-            J_trans = self.manip.CalculateJacobian()
-            J_quat = self.manip.CalculateRotationJacobian()
-
-        if currentpose[0] < 0:
-            currentpose[0:4] *= -1.
-            J_quat *= -1.
-
-        error_rot = self._eval_error_rotation(targetpose, currentpose)
-        error_pos = self._eval_error_position(targetpose, currentpose)
-        error = np.hstack([error_rot, error_pos])
-
-        J = np.vstack([J_quat[1:, :], J_trans])
-        try:
-            qd = np.linalg.solve(J, error)
-        except:
-            qd = np.dot(np.linalg.pinv(J), error)
-        return qd
-
--- tests/test_diffik.py
+++ tests/test_diffik.py
@@ -3,9 +3,9 @@ import random
 import time
 import numpy as np
 from EnvironmentSetup import OpenRAVEsetup
+from openravepy import matrixFromPose
 
 from iksolver.DiffIKSolver import DiffIKSolver
-from iksolver.DiffIKSolver import DiffIKSolver5D
 
 
 rng = random.SystemRandom()
@@ -22,6 +22,10 @@ class Test_DiffIksolver_Vanilla(OpenRAVEsetup):
         self.no_success = 0
         self.total_iter = 0
         self.no_total = 100
+        # A seed configuration is created by perturbing the true
+        # joints values with uniform noises in the range
+        # [-perturbation_mag, +perturbation_mag].
+        self.perturbation_mag = 0.1
 
         # Construct random test case
         self.qseeds = []
@@ -40,7 +44,8 @@ class Test_DiffIksolver_Vanilla(OpenRAVEsetup):
 
             # Perturb qseed from the desired position
             for k in xrange(ndof):
-                qseed[j] += (rng.random()*2 - 1) * .2
+                qseed[k] += (rng.random()*2 - 1) * self.perturbation_mag
+            qseed = np.maximum(np.minimum(qseed, self.q_max), self.q_min)
             self.qseeds.append(np.asarray(qseed))
 
 
@@ -56,7 +61,7 @@ class Test_DiffIksolver_Vanilla(OpenRAVEsetup):
 
     def test_diffiksolver_vanilla_case_1(self):
         """
-        Easy test. Differential IK Solver (vanilla version; IK6D)
+        Easy test. Differential IK Solver (vanilla version; IK6D)\n
         """
         i = 0
         for (q, pose) in zip(self.qseeds, self.poses):
@@ -71,9 +76,9 @@ class Test_DiffIksolver_Vanilla(OpenRAVEsetup):
                 self.no_success += 1
                 self.total_iter += result[1]
                 
-                q = result[2]
+                sol = result[2]
                 with self.robot:
-                    self.robot.SetActiveDOFValues(q)
+                    self.robot.SetActiveDOFValues(sol)
                     pose_actual = self.manip.GetTransformPose()
                 if pose_actual[0] < 0:
                     pose_actual[:4] *= -1.
@@ -83,102 +88,6 @@ class Test_DiffIksolver_Vanilla(OpenRAVEsetup):
                 # print 'actual pose', pose_actual
 
                 np.testing.assert_allclose(pose_actual, pose, rtol=1e-5, atol=1e-5)
-                self.assertTrue((q <= self.q_max).all(), msg="Violate joint limits")
-                self.assertTrue((self.q_min <= q).all(), msg="Violate joint limits")
-
-
-class Test_DiffIksolver_5D(OpenRAVEsetup):
-
-    def setUp(self):
-        super(Test_DiffIksolver_5D, self).setUp()
-        self.q_min = self.robot.GetActiveDOFLimits()[0]
-        self.q_max = self.robot.GetActiveDOFLimits()[1]
-
-        self.total_time = 0.0
-        self.no_success = 0
-        self.total_iter = 0
-        self.no_total = 100
-
-        # Construct random test case
-        self.qseeds = []
-        self.poses = []
-        ndof = self.robot.GetActiveDOF()
-        for i in xrange(self.no_total):
-            qseed = []
-            for j in xrange(ndof):
-                qseed.append(rng.random()*(self.q_max[j] - self.q_min[j]) + self.q_min[j])
-            with self.robot:
-                self.robot.SetActiveDOFValues(qseed)
-                pose = self.manip.GetTransformPose()
-            if pose[0] < 0:
-                pose[:4] *= -1.
-            self.poses.append(pose)
-
-            # Perturb qseed from the desired position
-            for k in xrange(ndof):
-                qseed[j] += (rng.random()*2 - 1) * .2
-            self.qseeds.append(np.asarray(qseed))
-
-
-    def tearDown(self):
-        super(Test_DiffIksolver_5D, self).tearDown()
-
-        # Report
-        print "Successful instances: {0}/{1}".format(self.no_success, self.no_total)
-        if self.no_success > 0:
-            print "Average time: {0} sec.".format(self.total_time/self.no_success)
-            print "Average iterations: {0}".format(1.0*self.total_iter/self.no_success)
-
-
-    def test_iksolver_5d_case_1(self):
-        """
-        Easy test. Differential IK Solver 5D
-        """
-        i = 0
-        for (q, pose) in zip(self.qseeds, self.poses):
-            i += 1
-            iksolver = DiffIKSolver5D(self.robot, self.manip.GetName())
-            ts = time.time()
-            result = iksolver.solve(pose, q, .8*1., conv_tol=1e-8)
-            te = time.time()
-
-            if result[0]:
-                self.total_time += te - ts
-                self.no_success += 1
-                self.total_iter += result[1]
-                
-                q = result[2]
-                with self.robot:
-                    self.robot.SetActiveDOFValues(q)
-                    pose_actual = self.manip.GetTransformPose()
-                if pose_actual[0] < 0:
-                    pose_actual[:4] *= -1.
-                    
-                # Check direction
-                cos_alpha_desired = pose[0]
-                sin_alpha_desired = np.sqrt(1 - cos_alpha_desired**2)
-                direction_desired = pose[1:4] / sin_alpha_desired
-
-                cos_alpha_actual = pose_actual[0]
-                sin_alpha_actual = np.sqrt(1 - cos_alpha_actual**2)
-                direction_actual = pose_actual[1:4] / sin_alpha_actual
-
-                print 'iter', i
-                print 'desired direction', direction_desired
-                print 'actual direction', direction_actual
-                print 'desired position', pose[4:]
-                print 'actual position', pose_actual[4:]
-                print 'targetpose', pose
-                print 'actualpose', pose_actual
-                
-                np.testing.assert_allclose(direction_desired, direction_actual, 
-                                           rtol=1e-5, atol=1e-5)
-
-                # Check position
-                np.testing.assert_allclose(pose_actual[4:], pose[4:], 
-                                           rtol=1e-5, atol=1e-5)
-                
-                print 'q', q
-                self.assertTrue((q <= self.q_max).all(), msg="Violate joint limits")
-                self.assertTrue((self.q_min <= q).all(), msg="Violate joint limits")
+                self.assertTrue((sol <= self.q_max).all(), msg="Violate joint limits")
+                self.assertTrue((self.q_min <= sol).all(), msg="Violate joint limits")
 
