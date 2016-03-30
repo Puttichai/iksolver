be495ad52e490ed5cab86c91a7eb3b53a7892188
Puttichai <L.Puttichai@gmail.com>
2016-03-30 15:16:16 +0800

    added optional arguments

--- tests/EnvironmentSetup.py
+++ tests/EnvironmentSetup.py
@@ -7,17 +7,18 @@ rospack = rospkg.RosPack()
 
 class OpenRAVEsetup(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self, robotfile='/robots/denso-vs060.zae', manipulatorname='Flange',
+              collisioncheckername='ode'):
         self.env = orpy.Environment()
 
         # Get the DensoVS60 model - fast loading time
-        robot_path = rospack.get_path('cribus_openrave') + '/robots/denso-vs060.zae'
+        robot_path = rospack.get_path('cribus_openrave') + robotfile
         self.robot = self.env.ReadRobotURI(robot_path)
         self.env.Add(self.robot, True)
-        collisionchecker = orpy.RaveCreateCollisionChecker(self.env, 'ode')
+        collisionchecker = orpy.RaveCreateCollisionChecker(self.env, collisioncheckername)
         self.env.SetCollisionChecker(collisionchecker)
         self.robot.SetName('Denso')
-        self.manip = self.robot.GetManipulator('Flange')
+        self.manip = self.robot.GetManipulator(manipulatorname)
 
     def tearDown(self):
         self.env.Destroy()
